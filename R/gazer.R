0dcb0154a6c5f103262f3ebb3c87c2796bebd8b6
unknown <mahr@wisc.edu>
2015-07-29 10:58:38 -0500

    store (temporary) require statements in central file

--- R/gazer.R
+++ R/gazer.R
@@ -6,3 +6,7 @@
 #' @import ggplot2
 #' @import reshape2
 NULL
+
+# Temporary place to house require statements. The final package should not use
+# these, since the NAMESPACE directives handle relationships to other packages.
+require(pbkrtest)
