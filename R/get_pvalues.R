350c027b04caf723b26e608c6e9f5375ec4614c1
unknown <mahr@wisc.edu>
2015-07-29 10:59:36 -0500

    refactor p-values to avoid brittle switch statement

--- R/get_pvalues.R
+++ R/get_pvalues.R
@@ -1,47 +1,48 @@
 #' Get p-values for a multilevel regression model
-#' 
-#' Takes a model fit with lmer and (optionally) an estimation method, returns a 
-#' data frame containing fixed effect coefficient estimates, SE, t-values, and 
-#' p-values. Can use normal approximation, Kenward-Roger approximation for 
-#' degrees of freedom, or both. If Kenward-Roger approximation is used, result 
+#'
+#' Takes a model fit with lmer and (optionally) an estimation method, returns a
+#' data frame containing fixed effect coefficient estimates, SE, t-values, and
+#' p-values. Can use normal approximation, Kenward-Roger approximation for
+#' degrees of freedom, or both. If Kenward-Roger approximation is used, result
 #' also contains approximated degrees of freedom.
-#' 
+#'
 #' @param model a merMod model object fit by lmer
 #' @param method p-value estimation method, defaults to "normal", can also be
 #'   "KR" or "all"
-#' @return data frame containing fixed effect estimates, SE, t-values, (degrees 
+#' @return data frame containing fixed effect estimates, SE, t-values, (degrees
 #'   of freedom if appropriate) and p-values.
 #' @importFrom pbkrtest get_Lb_ddf
 #' @export
 #' @examples
+#' require(lme4)
 #' model <- lmer(Reaction ~ Days + (Days | Subject), sleepstudy)
 #' get_pvalues(model)
 #' # Not working with lme4 1.8?
 #' # get_pvalues(m.full, "KR")
 get_pvalues <- function(model, method = "normal") {
-  #extract fixed effect coefficients from model
+  # Extract fixed effect coefficients from model
   coefs <- data.frame(coef(summary(model)))
-  switch(method,
-         # normal approximation
-         normal = {coefs$p.normal <- get_pvalues_normal(coefs$t.value)
-                   coefs$star.normal <- p_star(coefs$p.normal)},
-         # K-R
-         KR = {
-         require(pbkrtest)
-         coefs$df.KR <- get_Lb_ddf(model, fixef(model))
-               coefs$p.KR <- get_pvalues_kr(coefs$t.value, coefs$df.KR)
-               # coefs$star.KR <- p_star(coefs$p.KR)},
-         # all
-         all = {coefs$p.normal <- get_pvalues_normal(coefs$t.value)
-                coefs$df.KR <- get_Lb_ddf(model, fixef(model))
-                coefs$p.KR <- get_pvalues_kr(coefs$t.value, coefs$df.KR)
-                # coefs$star.normal <- p_star(coefs$p.normal)
-                # coefs$star.KR <- p_star(coefs$p.KR)},
-         {warning("Invalid p-value estimation method. Use 'normal', 'KR'", 
-                  "or 'all'. Defaulting to normal approximation.")
-          coefs$p.normal <- get_pvalues_normal(coefs$t.value)
-          # coefs$star.normal <- p_star(coefs$p.normal)})
-         
+
+  # Check/warn if bad method name used
+  supported <- c("normal", "KR", "all")
+  if (!is.element(method, supported)) {
+    warning("Invalid p-value estimation method. Use 'normal', 'KR'",
+            "or 'all'. Defaulting to normal approximation.")
+    method <- "normal"
+  }
+
+  # Apply normal approx if asked for
+  if (method %in% c("normal", "all")) {
+    coefs$p.normal <- get_pvalues_normal(coefs$t.value)
+    coefs$p.normal.star <- p_star(coefs$p.normal)
+  }
+
+  # Apply KR if asked for
+  if (method %in% c("KR", "all")) {
+    coefs$df.KR <- get_Lb_ddf(model, fixef(model))
+    coefs$p.KR <- get_pvalues_kr(coefs$t.value, coefs$df.KR)
+  }
+
   coefs
 }
 
@@ -54,9 +55,9 @@ get_pvalues_kr <- function(ts, dfs) {
 }
 
 p_star <- function(pval){
-  star <- ifelse(pval < 0.001, "***", 
-                 ifelse(pval < 0.01, "**", 
-                        ifelse(pval < 0.05, "*", 
-                               ifelse(pval < 0.1, ".", "")))) 
+  star <- ifelse(pval < 0.001, "***",
+                 ifelse(pval < 0.01, "**",
+                        ifelse(pval < 0.05, "*",
+                               ifelse(pval < 0.1, ".", ""))))
   return(star)
 }
