98f9416a6ce225d2ca9d06b4617c2dabb33d04cd
Jason Geller <jgeller1@iastate.edu>
2018-06-25 16:08:52 -0500

    Fixed formatting

--- R/missing_pupil_count.R
+++ R/missing_pupil_count.R
@@ -1,38 +1,27 @@
-<<<<<<< HEAD
 #' calculated percent missing data by subject and by trial
-#' 
+#'
 #'
 #' renames column variables and makes them lower case
 #' places time in ms
-#'@param missingthresh user-specified threshold for missing data. 
-
-
-
-
-
-=======
+#'@param missingthresh user-specified threshold for missing data.
 #' calculate percent missing data by subject and by trial
-#' 
 #'
-#' 
-#'@param missingthresh user-specified threshold for missing data. 
-
->>>>>>> 4f541da4288d022bccaa99fb52a6e8e0f4e6af48
-missing_pupil_count <- function(datafile, missingthresh=.5) { 
-  countsbysubject <- ddply(datafile, .(subject), summarise, 
+#'
+#'
+#'@param missingthresh user-specified threshold for missing data.
+missing_pupil_count <- function(datafile, missingthresh=.5) {
+  countsbysubject <- ddply(datafile, .(subject), summarise,
                            missing = sum(is.na(pupil) ), samples = sum(!is.na(pupil)), total = length(pupil))
   countsbysubject$averageMissingSub <- (countsbysubject$missing / countsbysubject$total)
   #print # subjects excluded
-  countsbytrial <- ddply(datafile, .(subject, trial), summarise, 
+  countsbytrial <- ddply(datafile, .(subject, trial), summarise,
                          missing = sum(is.na(pupil) ), samples = sum(!is.na(pupil)), total = length(pupil))
   countsbytrial$averageMissingTrial <- (countsbytrial$missing / countsbytrial$total)
-<<<<<<< HEAD
   #trials excluded
   combineSub<-merge(datafile, countsbysubject[, c("subject", "averageMissingSub")], by="subject", all=TRUE)
   combinetrial<-merge(combineSub, countsbytrial[, c("subject", "trial", "averageMissingTrial")], by=c("subject", "trial"), all=TRUE)
   combinetrial<-subset(combinetrial, (averageMissingSub < missingthresh) & (averageMissingTrial < missingthresh))
   return(countsbysubject)
-=======
   greaterthan=subset(countsbytrial, countsbytrial$averageMissingTrial > missingthresh)
   prop<-length(greaterthan$trial)/length(countsbytrial$trial)
   #%trials excluded
@@ -42,5 +31,4 @@ missing_pupil_count <- function(datafile, missingthresh=.5) {
   combinetrial<-merge(combineSub, countsbytrial[, c("subject", "trial", "averageMissingTrial")], by=c("subject", "trial"), all=TRUE)
   combinetrial<-subset(combinetrial, (averageMissingSub < missingthresh) & (averageMissingTrial < missingthresh))
   return(combinetrial)
->>>>>>> 4f541da4288d022bccaa99fb52a6e8e0f4e6af48
 }
