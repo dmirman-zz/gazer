23df0bb8ebbaebda4e348fd5621391e534a609cc
Jason Geller <jgeller1@iastate.edu>
2018-06-25 17:14:07 -0500

    #clean-up

--- R/missing_pupil_count.R
+++ R/missing_pupil_count.R
@@ -13,22 +13,19 @@ missing_pupil_count <- function(datafile, missingthresh=.5) {
   countsbysubject <- ddply(datafile, .(subject), summarise,
                            missing = sum(is.na(pupil) ), samples = sum(!is.na(pupil)), total = length(pupil))
   countsbysubject$averageMissingSub <- (countsbysubject$missing / countsbysubject$total)
-  #print # subjects excluded
+
   countsbytrial <- ddply(datafile, .(subject, trial), summarise,
                          missing = sum(is.na(pupil) ), samples = sum(!is.na(pupil)), total = length(pupil))
   countsbytrial$averageMissingTrial <- (countsbytrial$missing / countsbytrial$total)
-  #trials excluded
-  combineSub<-merge(datafile, countsbysubject[, c("subject", "averageMissingSub")], by="subject", all=TRUE)
-  combinetrial<-merge(combineSub, countsbytrial[, c("subject", "trial", "averageMissingTrial")], by=c("subject", "trial"), all=TRUE)
-  combinetrial<-subset(combinetrial, (averageMissingSub < missingthresh) & (averageMissingTrial < missingthresh))
-  return(countsbysubject)
   greaterthan=subset(countsbytrial, countsbytrial$averageMissingTrial > missingthresh)
   prop<-length(greaterthan$trial)/length(countsbytrial$trial)
   #%trials excluded
   print(prop)
   print(countsbysubject$subject[countsbysubject$averageMissingSub> missingthresh])
+  #print # subjects excluded
   combineSub<-merge(datafile, countsbysubject[, c("subject", "averageMissingSub")], by="subject", all=TRUE)
   combinetrial<-merge(combineSub, countsbytrial[, c("subject", "trial", "averageMissingTrial")], by=c("subject", "trial"), all=TRUE)
-  combinetrial<-subset(combinetrial, (averageMissingSub < missingthresh) & (averageMissingTrial < missingthresh))
-  return(combinetrial)
+  combinetrial_threshold<-subset(combinetrial, (averageMissingSub < missingthresh) & (averageMissingTrial < missingthresh))
+  return(combinetrial_threshold)
 }
+
