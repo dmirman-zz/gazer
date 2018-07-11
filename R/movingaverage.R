f46b44fed61a6970dc5fa9e2a6742654b4778216
Jason Geller <jgeller1@iastate.edu>
2018-07-11 14:32:51 -0500

    Matt's moving average function

---
+++ R/movingaverage.R
@@ -0,0 +1,66 @@
+###############################################-
+# Moving average function
+#
+# Creates a moving average using a specified averageing window
+# if n is 5, then it uses the target value,
+# 2 preceding and 2 following.
+#
+######################################
+movingAverage <- function(x, n=3, centered=TRUE) {
+
+  if (centered) {
+    before <- floor  ((n-1)/2)
+    after  <- ceiling((n-1)/2)
+  } else {
+    before <- n-1
+    after  <- 0
+  }
+
+  # Track the sum and count of number of non-NA items
+  s     <- rep(0, length(x))
+  count <- rep(0, length(x))
+
+  # Add the centered data
+  new <- x
+  # Add to count list wherever there isn't a
+  count <- count + !is.na(new)
+  # Now replace NA_s with 0_s and add to total
+  new[is.na(new)] <- 0
+  s <- s + new
+
+  # Add the data from before
+  i <- 1
+  while (i <= before) {
+    # This is the vector with offset values to add
+    new   <- c(rep(NA, i), x[1:(length(x)-i)])
+
+    count <- count + !is.na(new)
+    new[is.na(new)] <- 0
+    s <- s + new
+
+    i <- i+1
+  }
+  #
+  # Add the data from after
+  i <- 1
+  while (i <= after) {
+    # This is the vector with offset values to add
+    new   <- c(x[(i+1):length(x)], rep(NA, i))
+
+    count <- count + !is.na(new)
+    new[is.na(new)] <- 0
+    s <- s + new
+
+    i <- i+1
+  }
+
+  # return sum divided by count
+  s/count
+}
+#####################
+# Clean up leading NAs in a vector
+# by replacing the first 6 elements (100 ms) with the man value
+cleanUpLeadingNAs <- function(x){
+  x[1:6] <- mean(x, na.rm=TRUE)
+  return(x)
+}
