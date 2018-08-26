21eb78af1fe256fb6f481ce2d85b20b125395eb8
Jason Geller <jgeller1@iastate.edu>
2018-08-26 13:43:24 -0500

    changed to work with tidyverse

--- R/merge_pupil.R
+++ R/merge_pupil.R
@@ -9,9 +9,9 @@
 merge_pupil_files <- function (file_list) {  #file list is path to .xls files
   dataset <- do.call("rbind", lapply(file_list, FUN=function(files){
     read.table(files, header=TRUE, sep="\t", na.strings = ".") } ))
-  change_name <- rename(dataset,
-                        c(RECORDING_SESSION_LABEL="subject", TRIAL_INDEX="trial",
-                         AVERAGE_IN_BLINK="blink", AVERAGE_PUPIL_SIZE="pupil"))
+  change_name <- select(dataset,
+                        c(subject=RECORDING_SESSION_LABEL, trial = TRIAL_INDEX,
+                         blink = AVERAGE_IN_BLINK, pupil = AVERAGE_PUPIL_SIZE, everything()))
   names(change_name) <- tolower(names(change_name))
   change_name$time <- change_name$timestamp-change_name$ip_start_time
   return(change_name)
