fe0d3394bac8e2f8d6c65248dca37453ee99a2f7
Jason Geller <jgeller1@iastate.edu>
2018-09-26 16:25:28 -0500

    fix missing pupil

--- R/missing_pupil_count.R
+++ R/missing_pupil_count.R
@@ -3,38 +3,47 @@
 #' @export
 #' @return data frame containing missing data information
 #' @export
-missing_pupil_count <- function(datafile, missingthresh=.3) {
-
+#' 
+#' 
+#' 
+#' 
+#' 
+#' 
+#' 
+#' 
+missing_pupil_count<- function(datafile, missingthresh=.3) {
+  
   countsbysubject <- datafile %>%
-      dplyr::group_by(subject) %>%
-      dplyr::summarise(missing = sum(is.na(pupil)),
-            samples = sum(!is.na(pupil)),
-            total = length(pupil)) %>%
-      dplyr::mutate(averageMissingSub = missing / total)
-
+    dplyr::group_by(subject) %>%
+    dplyr::summarise(missing = sum(is.na(pupil)),
+                     samples = sum(!is.na(pupil)),
+                     total = length(pupil)) %>%
+    dplyr::mutate(averageMissingSub = missing / total)
+  
   countsbytrial <- datafile %>%
-      dplyr::group_by(subject, trial) %>%
-      dplyr::summarise(missing = sum(is.na(pupil)),
-            samples = sum(!is.na(pupil)),
-            total = length(pupil))%>%
-      dplyr::mutate(averageMissingTrial = missing / total)
-
+    dplyr::group_by(subject, trial) %>%
+    dplyr::summarise(missing = sum(is.na(pupil)),
+                     samples = sum(!is.na(pupil)),
+                     total = length(pupil))%>%
+    dplyr::mutate(averageMissingTrial = missing / total)
+  
   greaterthan <- filter(countsbytrial, averageMissingTrial > missingthresh)
   prop <- length(greaterthan$trial)/length(countsbytrial$trial)
-
+  
   # % trials excluded
   message(prop ,"of the trials were excluded")
   message("subjects taken out:" ,countsbysubject$subject[countsbysubject$averageMissingSub> missingthresh])
-
+  
   # print # subjects excluded
-
+  
   # add missing data per subject to the data frame
   combineSub <- merge(datafile, countsbysubject[, c("subject", "averageMissingSub")], by="subject", all=TRUE)
   # add missing data per trial to the data frame
   combinetrial <- merge(combineSub, countsbytrial[, c("subject", "trial", "averageMissingTrial")], by=c("subject", "trial"), all=TRUE)
   # keep only the data that exceeds the threshold for retaining the trials & subjects
   combinetrial_above_threshold <- filter(combinetrial, (averageMissingSub < missingthresh) & (averageMissingTrial < missingthresh))
-
+  
   return(combinetrial_above_threshold)
 }
+
 
