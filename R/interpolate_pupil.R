da62e7924f3b336edd2fdac12fc57d074e2b3f38
Jason Geller <jgeller1@iastate.edu>
2018-10-01 16:29:54 -0500

    updated interpolate function to incorporate extendblinks

--- R/interpolate_pupil.R
+++ R/interpolate_pupil.R
@@ -1,12 +1,14 @@
 #' Turns pupil sizes with blinks NA and linearly interpolates values
 #' @param datafile data frame.
+#' @param extendblinks blinks already extended  in data frame 
 #' @export
 #' @import zoo
 #' 
 #' @return data frame containing interpolated data
 #' 
-interpolate_pupil<-function(datafile) { 
+interpolate_pupil<-function(datafile, extendblinks=FALSE) { 
   require(zoo)
+  if (extendblinks==FALSE) {
 message("Turning pupil size with blinks to NA")
   blinks_na <- datafile %>% dplyr::mutate(pup = ifelse(blink==1, NA, pupil)) #turns blinks into NA for interpolation
   message("Performing linear interpolation")
@@ -14,4 +16,15 @@ message("Turning pupil size with blinks to NA")
     dplyr::group_by(subject, trial) %>%
     dplyr::mutate(interp = na.approx(pup, rule=2)) %>% ungroup() 
     return(pupil_interp)
+  }
+  
+  if (extendblinks==TRUE)
+    message("Extending blinks")
+message("Performing linear interpolation")
+pupil_interp <- data_frame() %>%
+  dplyr::group_by(subject, trial) %>%
+  dplyr::mutate(interp = na.approx(pupil, rule=2)) %>% ungroup() 
+return(pupil_interp)
 }
+
+  
