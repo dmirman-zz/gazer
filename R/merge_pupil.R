18710baa994c0dd34541c00944cff432ab359938
Jason Geller <jgeller1@iastate.edu>
2019-01-19 15:24:41 -0600

    make reading in pupil files faster with fread

--- R/merge_pupil.R
+++ R/merge_pupil.R
@@ -4,15 +4,15 @@
 #' Places time in ms
 #'@import tidyverse
 #'@import itsadug
+#'@import data.table
 #'
 #' @param file_list path to .xls files
 #' @export
 #' @return data frame containing pupil data
 merge_pupil_files <- function (file_list) {  #file list is path to .xls files
-  dataset <- do.call("rbind", lapply(file_list, FUN=function(files){
-    read.table(files, header=TRUE, sep="\t", na.strings = ".") } ))
-  change_name <- select(dataset,subject=RECORDING_SESSION_LABEL, trial = TRIAL_INDEX,
-                         blink = AVERAGE_IN_BLINK, pupil = AVERAGE_PUPIL_SIZE, everything())
+  library(data.table)
+  dataset <- do.call("rbind", lapply(file_list, fread)) #fread makes reading in files quicker
+  change_name <- select(dataset,subject=RECORDING_SESSION_LABEL, trial =  TRIAL_INDEX, blink = AVERAGE_IN_BLINK, pupil = AVERAGE_PUPIL_SIZE, everything())
   names(change_name) <- tolower(names(change_name))
   change_name$time <- change_name$timestamp-change_name$ip_start_time
   return(change_name)
