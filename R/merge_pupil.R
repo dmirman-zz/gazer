999732aaf179083f91ef74229e6f35e1e5e1c64f
Jason Geller <jgeller1@iastate.edu>
2019-01-19 16:24:06 -0600

    make merge faster

--- R/merge_pupil.R
+++ R/merge_pupil.R
@@ -11,7 +11,8 @@
 #' @return data frame containing pupil data
 merge_pupil_files <- function (file_list) {  #file list is path to .xls files
   library(data.table)
-  dataset <- do.call("rbind", lapply(file_list, fread)) #fread makes reading in files quicker
+  dataset <- do.call("rbind", lapply(file_list, FUN=function(files){
+    fread(files, header=TRUE, sep="\t", na.strings = ".")})) #fread makes reading in files quicker
   change_name <- select(dataset,subject=RECORDING_SESSION_LABEL, trial =  TRIAL_INDEX, blink = AVERAGE_IN_BLINK, pupil = AVERAGE_PUPIL_SIZE, everything())
   names(change_name) <- tolower(names(change_name))
   change_name$time <- change_name$timestamp-change_name$ip_start_time
