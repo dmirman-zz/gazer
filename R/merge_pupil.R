880f9c8fced88f4800532df8f771386f9f366a64
Jason Geller <jgeller1@iastate.edu>
2019-01-24 17:57:24 -0600

    added some other functionality

--- R/merge_pupil.R
+++ R/merge_pupil.R
@@ -11,11 +11,23 @@
 #' @return data frame containing pupil data
 merge_pupil_files <- function (file_list) {  #file list is path to .xls files
   library(data.table)
-  dataset <- do.call("rbind", lapply(file_list, FUN=function(files){
-    fread(files, header=TRUE, sep="\t", na.strings = ".")})) #fread makes reading in files quicker
-  change_name <- select(dataset,subject=RECORDING_SESSION_LABEL, trial =  TRIAL_INDEX, blink = AVERAGE_IN_BLINK, pupil = AVERAGE_PUPIL_SIZE, everything())
-  names(change_name) <- tolower(names(change_name))
-  change_name$time <- change_name$timestamp-change_name$ip_start_time
-  return(change_name)
-}
 
+    
+  dataset <- do.call("rbind", lapply(file_list, FUN=function(files){
+    
+    for (i in 1:length(files)){ 
+      if(file.exists(files[i])){
+        message( "now processing:", files[i])
+      }
+    }
+    
+    fread(files, header=TRUE, sep="\t", na.strings = ".")})) #fread makes reading in files quicke
+  
+  change_name <- select(dataset,subject=RECORDING_SESSION_LABEL, trial =  TRIAL_INDEX,    blink = AVERAGE_IN_BLINK, pupil = AVERAGE_PUPIL_SIZE, everything())
+  
+  names(change_name) <- tolower(names(change_name))
+  
+  change_name$time <- change_name$timestamp-change_name$ip_start_time
+  
+  return(as_tibble(change_name))
+}
