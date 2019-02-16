a1ef3975305b655a0efc2d8f1f1ce2da32644b3d
Jason Geller <jgeller1@iastate.edu>
2019-02-16 17:21:33 -0600

    arrange col

--- R/baseline_correction_pupil.R
+++ R/baseline_correction_pupil.R
@@ -7,15 +7,15 @@ baseline_correction_pupil<-function(datafile,baseline_window=NA) { message("Calc
   #recent paper (Mathot et al.,2018)suggested using median over mean for baseline correction
   message("Calculating median baseline from",":", baseline_window[1], "-", baseline_window[2])
   baseline <- datafile  %>%
-    dplyr::filter(timebins > baseline_window[1],
-                  timebins < baseline_window[2]) %>%
+    dplyr::filter(time > baseline_window[1],
+                  time < baseline_window[2]) %>%
     dplyr::group_by(subject, trial) %>%
     dplyr::summarise(baseline = median(movingavgpup)) %>% ungroup()
   message("Merging baseline")
   merge_baseline <- merge(baseline, datafile) # merge median pupil size with raw dataset
   message("Performing baseline correction")
   corrected_baseline <- merge_baseline %>%  
-    dplyr::mutate(baselinecorrectedp = movingavgpup - baseline) %>%       dplyr::arrange(subject, trial, timebins1, baselinecorrectedp)
+    dplyr::mutate(baselinecorrectedp = movingavgpup - baseline) %>%       dplyr::arrange(subject, trial, time, timebins, baselinecorrectedp)
   
   return(corrected_baseline)
 
