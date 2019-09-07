f247844b085b609dfc70bd239d6d6a603005f6d6
jgeller112 <jason-geller@uiowa.edu>
2019-09-07 12:45:52 -0500

    fix edf parse

--- R/parse_pupil_edf.R
+++ R/parse_pupil_edf.R
@@ -21,29 +21,28 @@ parse_pupil_edf <- function (file_list, output.dir) {
     
     for (sub in 1:subs) { 
       
-     samps_all <- edf.trials(file_list[sub], samples=TRUE)
-     
-     msg<-samps_all$messages
-     
+     #samps_all <- edf.trials(file_list[sub], samples=TRUE)
+     samps_all <- edf.batch(file_list[sub], samples = TRUE)
+     msg<-samps_all[[1]][["messages"]]
      #msg<-edf.messages.c(file_list[subs])
      msg<-dplyr::rename(msg, time="sttime")
-     samp<-samps_all$samples
-
-     dat_samp_msg <- merge(samp, msg, by=c("time", "eyetrial"), all=TRUE)
+     samp <- samps_all[[1]][["samples"]]
+     dat_samp_msg <- merge(samp, msg, by=c("ID", "time", "eyetrial"), all=TRUE)
      
      dat_samp_msg1 <- dat_samp_msg %>% 
-       #plyr::filter(eyetrial!="NA")%>% #non-trial value
-       dplyr::mutate(subject=str_replace(subj, pattern="/Users/gellr/Desktop/TL_EDF/", replacement = ""))
+       #plyr::filter(eyetrial!="NA")%>% #non-trial valu
        rowwise() %>% 
        dplyr::mutate(pupil=mean(c(paL,paR),na.rm=TRUE), gazex=mean(c(gxL,gxR),na.rm=TRUE), gazey=mean(c(gyL, gyR),na.rm=TRUE)) %>% # get if recorded from left return left if right right if both average averagepupil
-       dplyr::rename(trial="eyetrial") %>% 
-       dplyr::select(time, trial, pupil, gazex, gazey,trial, message) %>%
+       dplyr::rename(trial="eyetrial", subject="ID") %>% 
+       dplyr::select(subject, time, trial, pupil, gazex, gazey,trial, message) %>%
        ungroup() %>%
        dplyr::group_by(trial) %>%
        dplyr::mutate(time=time-time[1])
-    
+      
+      dat_samp_msg1$message <- gsub("[^a-zA-Z]", "", dat_samp_msg$message) #edf file has diff number before each message which makes it diff to align events
+     
       setwd(output.dir) 
-      subOutData <- paste(file_list[sub], "_raw.csv", sep="") # save file 
+      subOutData <- paste(file_list[sub], "_raw1.csv", sep="") # save file 
       write.table(dat_samp_msg1, file = subOutData, append = FALSE, sep = ",",
       row.names = FALSE, col.names = TRUE)
     }
