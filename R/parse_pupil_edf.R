3daad7f0d9af9ce8352f93f35a5941ae1cf1c643
jgeller112 <jason-geller@uiowa.edu>
2019-09-12 23:54:04 -0500

    add different blink algo

--- R/parse_pupil_edf.R
+++ R/parse_pupil_edf.R
@@ -13,13 +13,15 @@
 #
 #' @param file_list directory to edf files
 #' @param output.dir directory to save new cleaned files
+#' @param type include whether you want to parse edf pupil data or vwp (fixations)
 #' @export
 
-parse_pupil_edf <- function (file_list, output.dir) {
+parse_edf <- function (file_list, output.dir, type="") {
   
   library(edfR)#use edfR to read in the edf files
   library(saccades)
   
+  if (type=="pupil") {
     subs <- length(file_list)
     
     for (sub in 1:subs) { 
@@ -43,28 +45,45 @@ parse_pupil_edf <- function (file_list, output.dir) {
        dplyr::rename(trial="eyetrial")
      
      samp$subject<-subject
+     samp$row_index<-as.numeric(row.names(samp))
      
-     samp1<-select(samp, time, subject, trial, x, y,pup)
+     samp1<-select(samp, time, subject, trial, x, y,pup, row_index)
      
-     blk <- saccades::detect.fixations(samp1) # get blinks using the saccades alg
+     df_blinks <- as.data.frame(samp1) %>%
+       # pass into a list
+       dplyr::summarise(row_index= list(based_noise_blinks_detection(as.matrix(samp1$pup), sampling_rate_in_hz=250))) %>%
+       unnest()
      
-     blk <- blk %>%
-       filter(event=="blink") %>%
-       gather(startend, time, start:end) # gather all the blinks
+     df_blinks$Label <- rep(c("Onset", "Offset"), length.out=nrow(df_blinks))
      
-     blk_merge<- merge(samp1, blk, by=c("trial", "time", "x", "y"), all=TRUE)
+     df_blinks_merge<-merge(samp1, df_blinks, by="row_index", all.x=TRUE)
      
-     blinks <- blk_merge %>% 
-       group_by(grp = cumsum(!is.na(startend))) %>% 
-       mutate(Label = replace(startend, first(startend) == 'start', 'start')) %>% #extends the start message forward until end message
+     blinks <- df_blinks_merge %>% 
+       group_by(grp = cumsum(!is.na(Label))) %>% 
+       mutate(Label = replace(Label, first(Label) == 'Onset', 'Onset')) %>% #extends the start message forward until end message
        ungroup() %>% 
        # label blinks as 1 
        dplyr::select(subject, trial, time, x, y, pup, Label, -grp)
     
+     #blk <- saccades::detect.fixations(samp1) # get blinks using the saccades alg
+     
+     #blk <- blk %>%
+     #  filter(event=="blink") %>%
+     #  gather(startend, time, start:end) # gather all the blinks
+     
+     #blk_merge<- merge(samp1, blinks, by=c("trial", "time", "x", "y"), all=TRUE)
+     
+     #blinks <- blk_merge %>% 
+     #  group_by(grp = cumsum(!is.na(startend))) %>% 
+     #  mutate(Label = replace(startend, first(startend) == 'start', 'start')) %>% #extends the start message forward until end message
+     #  ungroup() %>% 
+       # label blinks as 1 
+     #  dplyr::select(subject, trial, time, x, y, pup, Label, -grp)
+    
        #blinks$blink[is.na(blinks$blink)] <- 0 
      
      
-     dat_samp_msg <- merge(blinks, msg, by=c("subject", "time", "trial"), all=TRUE)
+     dat_samp_msg <- merge(blinks, msg, by=c("subject", "time", "trial"), all.x=TRUE)
      
      dat_samp_msg1 <- dat_samp_msg %>% 
        dplyr::mutate(blink=ifelse(!is.na(Label), 1, 0), pupil=ifelse(blink==1 | pup==0, NA, pup))%>%
@@ -88,4 +107,49 @@ parse_pupil_edf <- function (file_list, output.dir) {
       write.table(dat_samp_msg1, file = subOutData, append = FALSE, sep = ",",
       row.names = FALSE, col.names = TRUE)
     }
+  }
+ 
+    if (type=="vwp") { 
+      
+      subs <- length(file_list)
+      
+      for (sub in 1:subs) { 
+        
+        subject = basename(file_list[sub])
+        #samps_all <- edf.trials(file_list[sub], samples=TRUE)
+        samps_all <- edf.batch(file_list[sub], samples = TRUE, do.plot=FALSE)
+        
+        samp <- samps_all[[1]][["samples"]]
+        
+        samp <- samp %>%
+          rowwise() %>%
+          dplyr::mutate(pupil=mean(c(paL,paR),na.rm=TRUE), x=mean(c(gxL,gxR),na.rm=TRUE), y=mean(c(gyL, gyR),na.rm=TRUE)) %>%
+          dplyr::rename(trial="eyetrial") %>%
+          ungroup()%>%
+          dplyr::group_by(trial) %>%
+          dplyr::mutate(time=time-time[1])
+          
+        samp$subject<-subject
+        
+        samp1<-select(samp, time, subject, trial, x, y,pupil)
+        
+        fixations <- dplyr::filter(detect.fixations(samp1), event=="fixation")
+        
+        fixations <- dplyr::rename(fixations, CURRENT_FIX_START="start", CURRENT_FIX_END="end", CURRENT_FIX_DURATION="dur", CURRENT_FIX_Y="y", CURRENT_FIX_X="x")
+       
+        fixations$subject<-subject
+        
+        fixations <- dplyr::select(fixations, subject, trial, CURRENT_FIX_START,CURRENT_FIX_END, CURRENT_FIX_DURATION, CURRENT_FIX_Y, CURRENT_FIX_X)
+        
+        setwd(output.dir) 
+        subOutData <- paste(file_list[sub], "_raw_vwp.csv", sep="") # save file 
+        write.table(fixations, file = subOutData, append = FALSE, sep = ",",
+                    row.names = FALSE, col.names = TRUE)
+        
+      }
+    
+    }
 }
+    
+  
+   
