fa84ede1e633370a03b239890d7609dd245f6383
jgeller112 <jason-geller@uiowa.edu>
2019-10-29 14:35:49 -0500

    merge smoothing and interpolation

---
+++ R/smooth_interpolate_pupil.R
@@ -0,0 +1,93 @@
+#' Smooths the pupil trace before linear or cubic interpolation.
+#' @param datafile data frame.
+#' @param extendpupil name of pupil col that was extended
+#' @param extendblinks blinks already extended  in data frame
+#' @param type type of interpolation (linear or cubic)
+#' @param maxgap max number of NAs to interpolate.
+#' @param hz recording frequency of ET
+#' @param n moving average window
+#' @export
+#' @import zoo
+#'
+#' @return df containing interpolated data
+#'
+smooth_interpolate_pupil<-function(datafile, pupil="pupil", extendpupil="extendpupil", extendblinks=FALSE, maxgap=Inf, type=NULL, hz=NA, n=NA) {
+  #supports linear and cublic-spline interpolation
+  if (maxgap!=Inf){
+    maxgap <- round(maxgap/(1000/hz))
+  }
+
+  if (extendblinks==FALSE & type=="linear") {
+
+    message("Turning pupil size with blinks to NA")
+
+    blinks_na <- datafile %>% dplyr::mutate(pup = ifelse(blink==1, NA, pupil)) #turns blinks into NA for interpolation
+
+    message("Smoothing the pupil trace with moving average")
+
+    smooth_pupil <- as.data.frame(blinks_na) %>%
+      mutate(movingavgpup =moving_average_pupil(pup, n = n))
+
+    message("Performing linear interpolation")
+
+    pupil_interp <- smooth_pupil %>%
+      dplyr::group_by(subject, trial) %>%
+      dplyr::mutate(interp = zoo::na.approx(movingavgpup, rule=2)) %>%
+      dplyr::ungroup()
+    return(pupil_interp)
+  }
+
+  if (extendblinks==TRUE & type=="linear") {
+
+    message("smoothing the pupil trace with moving average")
+
+    smooth_pupil <- as.data.frame(datafile) %>%
+      mutate(movingavgpup = moving_average_pupil(extendpupil, n = n))
+
+
+    message("Performing linear interpolation")
+    pupil_interp <- smooth_pupil %>%
+      dplyr::group_by(subject, trial) %>%
+      dplyr::mutate(interp = zoo::na.approx(extendpupil, na.rm = FALSE, rule=2)) %>%
+      dplyr::ungroup()
+    return(pupil_interp)
+
+  }
+
+  if (extendblinks==FALSE & type=="cubic") {
+    message("Turning pupil size with blinks to NA")
+    blinks_na <- datafile %>% dplyr::mutate(pup = ifelse(blink==1, NA, pupil)) #turns blinks into NA for interpolation
+
+    message("Smoothing the pupil trace with moving average")
+
+    smooth_pupil <- as.data.frame(blinks_na) %>%
+      mutate(movingavgpup =moving_average_pupil(pup, n = n))
+
+    message("Performing cubic interpolation")
+    pupil_interp <- smooth_pupil %>% dplyr::group_by(subject, trial) %>%
+      dplyr::mutate(index = ifelse(is.na(pup), NA, dplyr::row_number()),index=zoo::na.approx(index, na.rm=FALSE, rule=2),
+                    interp = zoo::na.spline(pup, na.rm=FALSE, x=index, maxgap=maxgap)) %>%
+      dplyr::ungroup()
+
+    return(pupil_interp)
+  }
+
+  if (extendblinks==TRUE & type=="cubic") {
+
+    message("Smoothing the pupil trace with moving average")
+
+    smooth_pupil <- as.data.frame(datafile) %>%
+      mutate(movingavgpup = moving_average_pupil(extendpupil, n = n))
+
+    message("Performing cubic interpolation")
+    pupil_interp <- datafile %>% dplyr::group_by(subject, trial) %>%
+      dplyr::mutate(
+        index = ifelse(is.na(extendpupil), NA, dplyr::row_number()),
+        index= zoo::na.approx(index, na.rm=FALSE),
+        interp = zoo::na.spline(extendpupil, na.rm=FALSE, x=index, maxgap=maxgap)) %>%
+      dplyr::ungroup()
+
+    return(pupil_interp)
+  }
+
+}
