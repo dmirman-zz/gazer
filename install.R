586576ea347fe60b31d24ca4c3ead9d883cac3ee
jgeller112 <jason-geller@uiowa.edu>
2019-11-03 08:20:04 -0600

    delete install.r

--- install.R
+++
@@ -1,9 +0,0 @@
-# The file structure and needed files should be set up from zip file
-
-# Download needed packages
-
-install.packages("tidyverse") # Download and install tidyverse
-install.packages("ggplot2")
-install.packages("zoo")
-install.packages("magrittr")
-install.packages("data.table")
