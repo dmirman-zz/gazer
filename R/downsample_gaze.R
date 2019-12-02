ee7d32783186dc226b49f73d765415e539d6bad1
Dan Mirman <dmirman@users.noreply.github.com>
2019-12-02 21:05:50 +0000

    added downsample_gaze

---
+++ R/downsample_gaze.R
@@ -0,0 +1,21 @@
+#' Downsample gaze data
+#'
+#' This function will combine gaze samples into time bins
+#' @param dataframe dataframe
+#' @param bin.length Length of time bins
+#' @param aggvars list of variable names to group by for aggregation
+#' @export
+downsample_gaze <- function(dataframe, bin.length = 50, timevar = "time", aggvars = list("subject", "condition", "target", "trial", "object", "timebin")){
+  downsample <- dataframe %>%
+    mutate(timebin = round(!!sym(timevar)/bin.size)*bin.size)
+  # if there are aggregation variables, then aggregate
+  if(length(aggvars > 0)){
+    downsample <- downsample %>%
+      dplyr::group_by_(.dots = aggvars) %>%
+      dplyr::summarize(acc = unique(acc), rt = unique(rt),
+                       Fix = mean(Fix) > 0.5) %>%
+      ungroup()
+  }
+  return(downsample)
+}
+
