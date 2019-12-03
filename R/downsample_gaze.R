ab6584f2ad5a17438bd252684e3faa62ee2f43f6
jgeller112 <jason-geller@uiowa.edu>
2019-12-02 19:07:31 -0600

    list to vector of variable names

--- R/downsample_gaze.R
+++ R/downsample_gaze.R
@@ -3,9 +3,9 @@
 #' This function will combine gaze samples into time bins
 #' @param dataframe dataframe
 #' @param bin.length Length of time bins
-#' @param aggvars list of variable names to group by for aggregation
+#' @param aggvars vector of variable names to group by for aggregation
 #' @export
-downsample_gaze <- function(dataframe, bin.length = 50, timevar = "time", aggvars = list("subject", "condition", "target", "trial", "object", "timebin")){
+downsample_gaze <- function(dataframe, bin.length = 50, timevar = "time", aggvars = c("subject", "condition", "target", "trial", "object", "timebin")){
   downsample <- dataframe %>%
     mutate(timebin = round(!!sym(timevar)/bin.length)*bin.length)
   # if there are aggregation variables, then aggregate
@@ -14,7 +14,7 @@ downsample_gaze <- function(dataframe, bin.length = 50, timevar = "time", aggvar
       dplyr::group_by_(.dots = aggvars) %>%
       dplyr::summarize(acc = unique(acc), rt = unique(rt),
                        Fix = mean(Fix) > 0.5) %>%
-      ungroup()
+      dplyr::ungroup()
   }
   return(downsample)
 }
