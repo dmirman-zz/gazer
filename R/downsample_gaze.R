cb097b42845d55b2b92f42a2616488d0a815942e
jgeller112 <jason-geller@uiowa.edu>
2019-12-03 22:22:52 -0600

    merge downsample_gaze and downsample_pupil

--- R/downsample_gaze.R
+++ R/downsample_gaze.R
@@ -5,8 +5,10 @@
 #' @param bin.length Length of time bins
 #' @param timevar time column
 #' @param aggvars vector of variable names to group by for aggregation
+#' @param type pupil for pupil data gaze for gaze data
 #' @export
-downsample_gaze <- function(dataframe, bin.length = 50, timevar = "time", aggvars = c("subject", "condition", "target", "trial", "object", "timebins")){
+downsample_gaze <- function(dataframe, bin.length = 50, timevar = "time", aggvars = c("subject", "condition", "target", "trial", "object", "timebins"), type="gaze"){
+  if(type=="gaze") { 
   downsample <- dataframe %>%
     mutate(timebins = round(!!sym(timevar)/bin.length)*bin.length)
   # if there are aggregation variables, then aggregate
@@ -19,4 +21,18 @@ downsample_gaze <- function(dataframe, bin.length = 50, timevar = "time", aggvar
   }
   return(downsample)
 }
+  if (type=="pupil") {
+    downsample <- dataframe %>%
+      mutate(timebins = round(!!sym(timevar)/bin.length)*bin.length)
+    # if there are aggregation variables, then aggregate
+    if(length(aggvars > 0)){ 
+      downsample <- downsample %>%
+        dplyr::group_by_(.dots = aggvars) %>%
+        dplyr::summarize(aggbaseline=mean(baselinecorrectedp)) %>%
+        dplyr::ungroup()
+    }
+  }
+  return(downsample)
+}
+
 
