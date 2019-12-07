a8787b0a46c863310d6c96a5feb237dd74a8b100
jgeller112 <jason-geller@uiowa.edu>
2019-12-07 14:52:43 -0600

    Fixed rolling merge issues. Now messages merged to closest value in sample report.

--- R/parse_edf.R
+++ R/parse_edf.R
@@ -1,10 +1,10 @@
 #' Take EDF files and return data that is in format usable for gazeR
-#' for pupil merges the samples data
+#' for pupil merges the sample data with the message data (merges on nearest values (millisecond diff between SR and this function)
 #' puts time in ms
 #' adds subject variable column
 #' cleans up the column names
 #' creates pupil column that is ambigious as to whether you sampled from left eye, right eye, or both (takes the average)
-#' For fixation data, obtains sample report, puts time in ms
+#' For fixation data, obtains sample report and puts time in ms
 #'@import tidyverse
 #'@import data.table
 #'@import edfR
@@ -14,107 +14,106 @@
 #' @export
 
 parse_edf <- function (file_list, output.dir, type="pupil") {
-  
+
   library(edfR)#use edfR to read in the edf files
   remotes::install_github("tmalsburg/saccades")
   library(saccades)
   library(data.table)
-  
+
   if (type=="pupil") {
     subs <- length(file_list)
-    
-    for (sub in 1:subs) { 
-      
+
+    for (sub in 1:subs) {
+
       subject = basename(file_list[sub])
-      samps_all <- edf.batch(file_list[sub], samples = TRUE, do.plot=FALSE)
-      
-      msg<-samps_all[[1]][["messages"]]
-      
+      all=edf.trials(file_list[sub], samples=TRUE)
+      #samps_all <- edf.batch(file_list[sub], samples = TRUE, do.plot=FALSE)
+
+      msg<-all$messages
+
       msg<- msg %>%
         dplyr::rename(trial="eyetrial", time="sttime") %>%
         distinct(time, .keep_all = TRUE)
-      
-      
-      samp <- samps_all[[1]][["samples"]]
-      
+
+
+      samp <- all$samples
+
       samp <- samp %>%
         rowwise() %>%
         dplyr::mutate(pup=mean(c(paL,paR),na.rm=TRUE), x=mean(c(gxL,gxR),na.rm=TRUE), y=mean(c(gyL, gyR),na.rm=TRUE)) %>%
         dplyr::rename(trial="eyetrial") %>%
-        dplyr::select(-blink, -fixation, -saccade) %>%
+        #dplyr::select(-blink, -fixation, -saccade) %>%
         dplyr::ungroup()
-      
+
       blinks_merge <- blink_detect(samp)
-      
-      blinks <- blinks_merge %>% 
-        dplyr::group_by(grp = cumsum(!is.na(startend))) %>% 
+
+      blinks <- blinks_merge %>%
+        dplyr::group_by(grp = cumsum(!is.na(startend))) %>%
         dplyr::mutate(Label = replace(startend, first(startend) == 'start', 'start')) %>% #extends the start message forward until end message
-        dplyr::ungroup() %>% 
-        # label blinks as 1 
+        dplyr::ungroup() %>%
+        # label blinks as 1
         dplyr::select(trial, time, x, y, pup, Label, -grp)
-      # use rolling merge from DT 
-      setDT(blinks)
-      setDT(msg)
-      
-      DT_mesg <- msg[blinks, on="time", roll="nearest"]
-      
-      #SR edfs are a nightmare. This makes it so messages are alined with closest values 
-      get_msg <- DT_mesg %>% 
-        group_by(trial, message) %>% 
+      # use rolling merge from DT
+
+      blinks <-as.data.table(blinks)
+      msg<-as.data.table(msg)
+      DT_mesg <- msg[blinks, on="time", roll=5]
+
+      #SR edfs are a nightmare. This makes it so messages are alined with closest values
+      get_msg <- DT_mesg %>%
+        group_by(trial, message) %>%
         top_n(n=1, wt=desc(time))
-      
-      blinks=as_tibble(blinks)
-      
-      dat_samp_msg <- base::merge(blinks, get_msg, by=c("time", "trial","x", "y", "Label", "pup"), all=TRUE)
-      
-      dat_samp_msg1 <- dat_samp_msg %>% 
+
+      dat_samp_msg <- dplyr::full_join(blinks, get_msg, by=c("time", "trial", "x", "y", "pup", "Label"))
+
+      dat_samp_msg1 <- dat_samp_msg %>%
         dplyr::mutate(blink=ifelse(!is.na(Label), 1, 0), pupil=ifelse(blink==1 | pup==0, NA, pup))%>%
-        dplyr::filter(trial!="NA") %>% # get rid non-trial values 
+        dplyr::filter(trial!="NA") %>% # get rid non-trial values
         dplyr::group_by(trial) %>%
         dplyr::mutate(time=time-time[1], subject=subject)%>%
         dplyr::ungroup()%>%
         dplyr::select(subject, time, trial, pupil, x, y, trial, blink, message, -Label)
-      
+
       dat_samp_msg2 <- dat_samp_msg1 %>%
         group_by(subject) %>%
         distinct(trial, time, .keep_all=TRUE) %>%
         ungroup()
-      
-      
-      setwd(output.dir) 
-      subOutData <- paste(file_list[sub], "_raw_pupil.csv", sep="") # save file 
+
+
+      setwd(output.dir)
+      subOutData <- paste(file_list[sub], "_raw_pupil.csv", sep="") # save file
       write.table(dat_samp_msg2, file = subOutData, append = FALSE, sep = ",",
                   row.names = FALSE, col.names = TRUE)
     }
   }
-  
-  if (type=="vwp") { 
-    
+
+  if (type=="vwp") {
+
     subs <- length(file_list)
-    
-    for (sub in 1:subs) { 
-      
+
+    for (sub in 1:subs) {
+
       subject = basename(file_list[sub])
       #samps_all <- edf.trials(file_list[sub], samples=TRUE)
       samps_all <- edf.batch(file_list[sub], samples = TRUE, do.plot=FALSE)
-      
+
       samp <- samps_all[[1]][["samples"]]
-      
-      
-      df_samp = samp %>% group_by(eyetrial) %>% 
+
+
+      df_samp = samp %>% group_by(eyetrial) %>%
         dplyr::mutate(time=time-time[1]) %>%
         dplyr::rowwise() %>%
-        dplyr::mutate(pupil=mean(c(paL,paR),na.rm=TRUE), x=mean(c(gxL,gxR),na.rm=TRUE), y=mean(c(gyL, gyR),na.rm=TRUE)) %>% 
+        dplyr::mutate(pupil=mean(c(paL,paR),na.rm=TRUE), x=mean(c(gxL,gxR),na.rm=TRUE), y=mean(c(gyL, gyR),na.rm=TRUE)) %>%
         dplyr::select(-blink, -fixation, -saccade, -gxL, -gyL, -gxR, -gyR, -paR, -paL) %>%
         dplyr::rename(trial="eyetrial")
-      
-      setwd(output.dir) 
-      subOutData <- paste(file_list[sub], "_raw_vwp.csv", sep="") # save file 
+
+      setwd(output.dir)
+      subOutData <- paste(file_list[sub], "_raw_vwp.csv", sep="") # save file
       write.table(df_samp, file = subOutData, append = FALSE, sep = ",",
                   row.names = FALSE, col.names = TRUE)
-      
+
     }
-    
+
   }
 }
 
