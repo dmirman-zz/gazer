3b4fe62b64cf8ee2f9faba873953757ab0df677d
jgeller112 <jason-geller@uiowa.edu>
2020-07-03 01:00:30 -0400

    change order of abs and max in speed pupil

--- R/speed_pupil.R
+++ R/speed_pupil.R
@@ -9,8 +9,8 @@ speed_pupil <- function(pup, time) {
   backward_pupil<- c(NA, cur_dilation_speed)
   forward_pupil<-c(cur_dilation_speed,NA)
   backfwd_pupil<-cbind(backward_pupil, forward_pupil)
-  max_backfwd_pupil<-abs(max_backfwd_pupil)
-  max_backfwd_pupil<-apply(backfwd_pupil, 1,max, na.rm=TRUE)
+  max_backfwd_pupil<-abs(backfwd_pupil)
+  max_backfwd_pupil<-apply(max_backfwd_pupil, 1,max, na.rm=TRUE)
   return(max_backfwd_pupil)
 }
 
